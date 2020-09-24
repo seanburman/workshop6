a30a48c8bb8180d8e1292ed5e2b4e41ef21f6da6
Jessy Perreault <jessyperreault8@gmail.com>
2020-09-24 16:28:17 -0600

    changes Booking.java to BookingView.java, Adds Booking class, adds TripType class

--- src/views/BookingView.java
+++ src/views/BookingView.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Booking extends Application {
+public class BookingView extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
