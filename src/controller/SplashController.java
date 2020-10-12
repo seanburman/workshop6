595500dcc710e4f6ea8d60c39057cbf96ed99ff1
seanburman <sean.burman@icloud.com>
2020-10-12 15:31:10 -0600

    Added Splash Screen. Still investigating some animation to make this more tasty.

---
+++ src/controller/SplashController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class SplashController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private ImageView splashPlane;
+
+    @FXML
+    private Button btnBegin;
+
+    @FXML
+    void loadLogin(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("views/AgentLogin.fxml"), resources);
+        Parent root = (Parent) loader.load();
+        mainPane.getChildren().setAll(root);
+    }
+
+    @FXML
+    void initialize() {
+        assert mainPane != null : "fx:id=\"mainPane\" was not injected: check your FXML file 'Splash.fxml'.";
+        assert splashPlane != null : "fx:id=\"splashPlane\" was not injected: check your FXML file 'Splash.fxml'.";
+        assert btnBegin != null : "fx:id=\"btnBegin\" was not injected: check your FXML file 'Splash.fxml'.";
+
+    }
+}
+
