c400b318003b2f788484e1ca26f26b07baa0d9f7
seanburman <sean.burman@icloud.com>
2020-10-30 07:49:10 -0600

    Deleted buttons from Agent Profile Changed main title to Travel Experts Agent Portal

--- src/views/Main.java
+++ src/views/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Splash.fxml"));
         primaryStage.setTitle("Travel Experts Agent Portal");
+        primaryStage.getIcons().add(new Image("/images/agent.png"));
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.setResizable(false);
         primaryStage.show();
